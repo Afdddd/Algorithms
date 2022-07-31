458301564089b67f8e1977cc0bac7c9571545acb
inyeop <kimdlsduq36@gmail.com>
2022-07-31 16:05:24 +0900

    Basic_012

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float v = scanner.nextFloat();
-        System.out.format("%f",v);
+        int i = scanner.nextInt();
+        int i2 = scanner.nextInt();
+        System.out.format("%d %d" ,i,i2);
     }
 }
