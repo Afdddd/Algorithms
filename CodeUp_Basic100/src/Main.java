a68a0f06048b06b8a4b52d9bfa23e8e3533fbb86
inyeop <kimdlsduq36@gmail.com>
2022-07-31 17:07:00 +0900

    Basic_018

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringTokenizer str = new StringTokenizer(scanner.nextLine(),".");
-        int year = Integer.parseInt(str.nextToken());
-        int month = Integer.parseInt(str.nextToken());
-        int day = Integer.parseInt(str.nextToken());
-        System.out.format("%04d.%02d.%02d", year, month, day);
+        String num = scanner.nextLine();
+        String[] strings = num.split("-");
+        System.out.println(strings[0]+strings[1]);
     }
 }
