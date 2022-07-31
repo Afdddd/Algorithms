4557eb91e654687bdeb4d685f9dba26179a40df9
inyeop <kimdlsduq36@gmail.com>
2022-07-31 16:52:45 +0900

    Basic_017

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,10 +4,10 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringTokenizer str = new StringTokenizer(scanner.nextLine(),":");
-        String hour = str.nextToken();
-        String min = str.nextToken();
-        System.out.println(hour + ":" + min);
-
+        StringTokenizer str = new StringTokenizer(scanner.nextLine(),".");
+        int year = Integer.parseInt(str.nextToken());
+        int month = Integer.parseInt(str.nextToken());
+        int day = Integer.parseInt(str.nextToken());
+        System.out.format("%04d.%02d.%02d", year, month, day);
     }
 }
