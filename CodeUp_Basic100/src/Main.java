e9545c8635e65a4961fd357de754c76b0e0b3d08
inyeop <kimdlsduq36@gmail.com>
2022-07-31 16:31:14 +0900

    Basic_014

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringTokenizer stt = new StringTokenizer(scanner.nextLine());
-        String s1 = stt.nextToken();
-        String s2 = stt.nextToken();
-        System.out.print(s2+" "+s1);
-
+        float f = scanner.nextFloat();
+        System.out.format("%.2f", f);
 
     }
 }
