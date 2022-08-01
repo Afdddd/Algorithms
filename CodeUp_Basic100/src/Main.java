6e0c069504a30e3c8a70eb16e9608d39d719b2b1
inyeop <kimdlsduq36@gmail.com>
2022-08-01 23:02:46 +0900

    Basic_020

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String data[] = new String[51];
-        data[0]=scanner.nextLine();
-        System.out.println(data[0]);
+        String str = scanner.nextLine();
+        System.out.println(str);
+
     }
 }
