053a5f85ebd6091ddef1c4239903f168d5deeefd
inyeop <kimdlsduq36@gmail.com>
2022-08-02 10:56:38 +0900

    Basic_026

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next());
+        double v = sc.nextDouble();
+        System.out.format("%.11f",v);
+
     }
 }
