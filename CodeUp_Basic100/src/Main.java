51557b488bdce9df91cff4dba6653bfd27d76214
inyeop <kimdlsduq36@gmail.com>
2022-08-02 10:48:33 +0900

    Basic_025

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] strings = sc.next().split("\\.");
-        int yyyy = Integer.parseInt(strings[0]);
-        int mm = Integer.parseInt(strings[1]);
-        int dd = Integer.parseInt(strings[2]);
-        System.out.format("%02d-%02d-%04d", dd, mm, yyyy);
+        System.out.println(sc.next());
     }
 }
