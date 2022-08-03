8ac734056ba4b059283941bb67902951bd17cb3a
inyeop <kimdlsduq36@gmail.com>
2022-08-03 10:21:49 +0900

    Basic_031

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = Integer.toHexString(sc.nextInt());
-        System.out.println(s.toUpperCase());
+        Integer valueOf = Integer.valueOf(sc.next(), 8);
+        System.out.println(valueOf);
 
     }
 }
