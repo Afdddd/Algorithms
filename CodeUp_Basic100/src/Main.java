aa3d2eeca51729fe78773483dc4f47f38377bad8
inyeop <kimdlsduq36@gmail.com>
2022-08-03 10:28:49 +0900

    Basic_033

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer valueOf = Integer.valueOf(sc.next(), 16);
-        String toOctalString = Integer.toOctalString(valueOf);
-        System.out.println(toOctalString);
+        char c = sc.nextLine().charAt(0);
+        int num = (byte) c;
+        System.out.println(num);
 
     }
 }
