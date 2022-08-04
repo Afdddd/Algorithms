ce44311035f8a2a7d7e5b033a59cfa0d1d1e6c88
inyeop <kimdlsduq36@gmail.com>
2022-08-04 10:41:57 +0900

    Basic_041

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        System.out.println(num1%num2);
+        long num1 = sc.nextLong();
+        num1++;
+        System.out.println(num1);
 
     }
 }
