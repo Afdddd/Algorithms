93e93ea1236d710adc94c54e4540f845e59285c1
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:49:23 +0900

    Basic_060

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        System.out.println(num1>num2?num1:num2);
+        int num3 = sc.nextInt();
+
+        System.out.println(num1<num2?num1<num3?num1:num3:num2<num3?num2:num3);
 
     }
 }
