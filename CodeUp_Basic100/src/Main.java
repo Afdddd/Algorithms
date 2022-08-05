bc75e4fca10d59b850a40e28c5a4ef001b9251ba
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:58:50 +0900

    Basic_063

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] nums = sc.nextLine().split(" ");
-        for (int i = 0; i < nums.length; i++) {
-            int num = Integer.parseInt(nums[i]);
-            if (num % 2 == 0) {
-                System.out.println("even");
-            }else {
-                System.out.println("odd");
-            }
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println("plus");
+        }else {
+            System.out.println("minus");
+        }
+        if (num % 2 == 0) {
+            System.out.println("even");
+        }else {
+            System.out.println("odd");
         }
     }
 }
