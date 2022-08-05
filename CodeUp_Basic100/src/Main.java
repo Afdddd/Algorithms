bcaefc0fb74608eef58db829c94bb929074c0896
inyeop <kimdlsduq36@gmail.com>
2022-08-05 20:34:21 +0900

    Basic_043

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        System.out.println(num1+num2);
-        System.out.println(num1-num2);
-        System.out.println(num1*num2);
-        System.out.println(num1/num2);
-        System.out.println(num1%num2);
-        System.out.format("%.2f",num1/(float)num2);
+        int num3 = sc.nextInt();
+
+        int numAdd = num1+num2+num3;
+        float numAve = (float) numAdd/3;
+        System.out.println(numAdd);
+        System.out.format("%.1f",numAve);
     }
 }
