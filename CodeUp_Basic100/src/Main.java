44aaa2bdb88531c91513682d4e1e44f8ff5c2eb3
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:04:12 +0900

    Basic_045

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println(num<<1);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1<<num2);
     }
 }
+//a를 이진수로 101이라고 했을 때, <<1을 해주면 1010, <<2을 해주면 10100, ... 으로 한칸씩 늘어납니다.
+//n번 해주면 n번 왼쪽으로 밀릴 테니 2^n배가 되겠죠?
