a427fa5fb01546f7b748c3fd4d2eb0811a4ef533
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:14:37 +0900

    Basic_051

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if(num==0){
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if (num1 == 1 && num2 == 1) {
             System.out.println(1);
-        } else if (num==1) {
+        }else {
             System.out.println(0);
         }
     }
