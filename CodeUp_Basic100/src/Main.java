20d8216c21b7661f4c7edea9e31eadf9115c5572
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:38:38 +0900

    Basic_057

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
-        System.out.println(~num1);
+        int num2 = sc.nextInt();
+        System.out.println(num1&num2);
 
     }
 }
