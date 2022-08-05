103be43273c2a8e511af14b30222c9b12043535a
inyeop <kimdlsduq36@gmail.com>
2022-08-05 21:55:06 +0900

    Basic_061

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-
-        System.out.println(num1<num2?num1<num3?num1:num3:num2<num3?num2:num3);
-
+        String[] nums = sc.nextLine().split(" ");
+        for (int i = 0; i < nums.length; i++) {
+            int num = Integer.parseInt(nums[i]);
+            if (num % 2 == 0) {
+                System.out.println(nums[i]);
+            }
+        }
     }
 }
