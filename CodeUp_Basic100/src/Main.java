d53a30eac553d9891adbb51639c8504a442e30b2
inyeop <kimdlsduq36@gmail.com>
2022-08-05 20:39:58 +0900

    Basic_044

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,13 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-
-        int numAdd = num1+num2+num3;
-        float numAve = (float) numAdd/3;
-        System.out.println(numAdd);
-        System.out.format("%.1f",numAve);
+        int num = sc.nextInt();
+        System.out.println(num<<1);
     }
 }
