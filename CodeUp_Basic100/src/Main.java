fd5d62581a9759622a585833a528c2c4dce46801
inyeop <kimdlsduq36@gmail.com>
2022-08-05 22:06:27 +0900

    Basic_065

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if(90<=num&&num<=100){
-            System.out.println("A");
-        } else if(70<=num&&num<=89){
-            System.out.println("B");
-        } else if(40<=num&&num<=69){
-            System.out.println("C");
-        } else if(0<=num&&num<=39){
-            System.out.println("D");
+        String grade = sc.next();
+        switch (grade) {
+            case "A":
+                System.out.println("best!!!");
+                break;
+            case "B":
+                System.out.println("good!!");
+                break;
+            case "C":
+                System.out.println("run!");
+                break;
+            case "D":
+                System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
         }
     }
 }
