3c6a5cbdcc7306024030c32f9963606a9ab1b0a3
inyeop <kimdlsduq36@gmail.com>
2022-08-06 21:00:59 +0900

    Basic_069

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[] data = new int[num];
-        for (int i = 0; i < num; i++) {
-            data[i] = sc.nextInt();
-        }
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                break;
+            }
+            System.out.println(num);
         }
     }
 
