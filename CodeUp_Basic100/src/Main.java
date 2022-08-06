668800535895805c339799e589f41200559e4661
inyeop <kimdlsduq36@gmail.com>
2022-08-06 21:40:21 +0900

    Basic_074

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        int sum =0;
+
         for (int i = 0; i <= num; i++) {
-            System.out.println(i);
+            if (i % 2 == 0) {
+                sum+=i;
+                }
+            }
+        System.out.println(sum);
         }
 
     }
-}
