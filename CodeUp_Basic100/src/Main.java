b5f44f8822010a10a0f1a9c33546d7f7e59b4f8b
inyeop <kimdlsduq36@gmail.com>
2022-08-06 21:29:29 +0900

    Basic_072

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        while (true) {
-            --num;
-            if (num < 0) {
-                break;
-            }
-            System.out.println(num);
-
+        char c = sc.nextLine().charAt(0);
+        for (int i = (int)'a' ; i <= c; i++) {
+            char dd = (char) i;
+            System.out.print(dd+" ");
         }
-    }
 
+    }
 }
