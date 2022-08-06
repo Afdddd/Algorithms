dc9626d9cabda9402327c27e89d269e27aaf7e0c
inyeop <kimdlsduq36@gmail.com>
2022-08-06 21:06:11 +0900

    Basic_071

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -5,11 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         while (true) {
-            if (num == 0) {
+            --num;
+            if (num < 0) {
                 break;
             }
             System.out.println(num);
-            num--;
+
         }
     }
 
