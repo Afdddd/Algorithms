a4f7649de1087b1c78037481987b3f7f842ff05b
inyeop <kimdlsduq36@gmail.com>
2022-08-08 22:44:06 +0900

    Basic_078

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,15 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        for (int i = 1; i <= num1; i++) {
-            for (int j = 1; j <= num2; j++) {
-                System.out.printf("%d %d",i,j);
-                System.out.println();
-            }
+        int hex = sc.nextInt(16);
+        for (int i = 1; i < 16; i++) {
+            System.out.printf("%X*%X=%X\n",hex,i,hex*i);
         }
-
     }
 }
