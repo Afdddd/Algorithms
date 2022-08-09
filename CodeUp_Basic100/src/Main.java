0f1bb8e97376ece42b56c7ec5ff2f9da61f1deaa
inyeop <kimdlsduq36@gmail.com>
2022-08-09 21:26:34 +0900

    Basic_080

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -1,16 +1,25 @@
+import java.io.*;
 import java.util.Scanner;
-
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 1; i <= num; i++) {
-            if (i % 3 == 0) {
-                System.out.print("X ");
-            } else {
-                System.out.print(i+" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int c=0;
+        String[] split = br.readLine().split(" ");
+        int r = Integer.parseInt(split[0]);
+        int g = Integer.parseInt(split[1]);
+        int b = Integer.parseInt(split[2]);
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < g; j++) {
+                for (int k = 0; k < b; k++) {
+                    c++;
+                    bw.write(i+" "+j+" "+k+" \n");
+                }
             }
         }
+        bw.write(String.valueOf(c));
+        bw.flush();
     }
 }
