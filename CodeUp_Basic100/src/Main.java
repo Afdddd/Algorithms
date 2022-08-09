80db322327d657d3e303c682a877e3d518afd7e8
inyeop <kimdlsduq36@gmail.com>
2022-08-09 22:38:11 +0900

    Basic_081

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -1,25 +1,25 @@
 import java.io.*;
 import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println(1<<10);
+        Scanner sc = new Scanner(System.in);
+        String[] split = sc.nextLine().split(" ");
+        int h = Integer.parseInt(split[0]);
+        int b = Integer.parseInt(split[1]);
+        int c = Integer.parseInt(split[2]);
+        int s = Integer.parseInt(split[3]);
+        cal(h, b, c, s);
 
-        int c=0;
-        String[] split = br.readLine().split(" ");
-        int r = Integer.parseInt(split[0]);
-        int g = Integer.parseInt(split[1]);
-        int b = Integer.parseInt(split[2]);
+    }
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < g; j++) {
-                for (int k = 0; k < b; k++) {
-                    c++;
-                    bw.write(i+" "+j+" "+k+" \n");
-                }
-            }
-        }
-        bw.write(String.valueOf(c));
-        bw.flush();
+    public static void cal(int h, int b, int c, int s) {
+        double r = (double) h*b*c*s;
+        double pow = Math.pow(1024, 2);
+        double result = r / (pow*8);
+        System.out.printf("%.1f MB",result);
+
     }
 }
