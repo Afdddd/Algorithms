a9d74c64121fc89208e26c5a0edd81e911c43ced
inyeop <kimdlsduq36@gmail.com>
2022-08-10 23:45:11 +0900

    Basic_082

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(1<<10);
         Scanner sc = new Scanner(System.in);
         String[] split = sc.nextLine().split(" ");
-        int h = Integer.parseInt(split[0]);
-        int b = Integer.parseInt(split[1]);
-        int c = Integer.parseInt(split[2]);
-        int s = Integer.parseInt(split[3]);
-        cal(h, b, c, s);
+        int w = Integer.parseInt(split[0]);
+        int h = Integer.parseInt(split[1]);
+        int b = Integer.parseInt(split[2]);
+        cal(w, h ,b);
 
     }
 
-    public static void cal(int h, int b, int c, int s) {
-        double r = (double) h*b*c*s;
+    public static void cal(int w, int h, int b) {
+        double r = (double) w*h*b;
         double pow = Math.pow(1024, 2);
         double result = r / (pow*8);
-        System.out.printf("%.1f MB",result);
+        System.out.printf("%.2f MB",result);
 
     }
 }
