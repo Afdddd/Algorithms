3e60c2484cb90175f0746915cdf0f15a50382e37
inyeop <kimdlsduq36@gmail.com>
2022-08-11 10:50:35 +0900

    Basic_085

--- CodeUp_Basic100/src/Main.java
+++ CodeUp_Basic100/src/Main.java
@@ -6,14 +6,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= num; i++) {
-            if (i % 3 == 0) {
-                continue;
-            }
-            String s = String.valueOf(i);
-            bw.write(s+" ");
+        String[] nums = br.readLine().split(" ");
+        int a = Integer.parseInt(nums[0]);
+        int d = Integer.parseInt(nums[1]);
+        int n = Integer.parseInt(nums[2]);
+
+        for (int i = 1; i < n; i++) {
+            a+=d;
         }
+        bw.write(String.valueOf(a));
         bw.flush();
         bw.close();
     }
